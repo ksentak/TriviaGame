afc27482e7aa792c9114a5d9cd884aa2904410dc
KSentak <k.sentak@outlook.com>
2019-10-22 14:26:36 -0400

    Started to pseudocode my javascript page

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+//When start is clicked, trivia game begins
+
+//A timer appears at the top and begins to count down until question is answered or time is up
+
+//A question appears with 4 multiple choice selections to choose from
+
+//Answer buttons 1-4 match input from an array?
+//on click of one of the four answer buttons move on to the next question
+//if timer runs out move to next question
+
+//Reset timer each time a new question appears 
+
+
+// if statements
+
+//if answer is correct tally a correct answer
+//if answer is not correct tally an incorrect answer
+//if question is not answered tally an unanswered question
+
+//SDReyes github readme example
