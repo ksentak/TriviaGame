ef489fdd2802258ce735701e980ddd2a7c51429e
KSentak <k.sentak@outlook.com>
2019-10-23 15:37:24 -0400

    Created global variables and Q/A array + added a timer section to HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,125 @@
-//When start is clicked, trivia game begins
+$(document).ready(function() {
 
-//A timer appears at the top and begins to count down until question is answered or time is up
+//Define global variables
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unansweredQuestions = 0;
+
+//Create my array of questions
+
+var triviaQuestions = [
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: "",
+    },
+]
+
+//New Game function that hides timer/# of correct & incorrect answers
+function newGame() {
+    $("#timer").hide();
+    $("#questions").hide();
+    $("#results").hide();
+}
+
+
+
+//When start is clicked, the button disappears and trivia game begins 
+$("#startbtn").on("click", function() {
+    $("#startbtn").remove();
+    $("#letsplay").remove();
+    $("#timer").show();
+    $("#questions").show();
+
+//Also on click a timer appears at the top and begins to count down until questions are answered or time is up
+
+
+});
+
 
 //A question appears with 4 multiple choice selections to choose from
 
@@ -17,4 +136,5 @@
 //if answer is not correct tally an incorrect answer
 //if question is not answered tally an unanswered question
 
-//SDReyes github readme example
+});
+
